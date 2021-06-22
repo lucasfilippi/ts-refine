f240d2f13f1e602502056f4994415ea4d6147ea5
Lucas Filippi <lucasfilippi@protonmail.com>
2021-06-22 10:12:27 +0200

    Complete refactor: add Bitmap Index support for facets

--- src/lib/minifacet.spec.ts
+++ src/lib/minifacet.spec.ts
@@ -1,119 +1,155 @@
 import test, { ExecutionContext } from 'ava';
-import { SearchResult } from 'minisearch';
+import TypedFastBitSet from 'typedfastbitset';
+// import { SearchResult } from 'minisearch';
 
-import { FacetFilter, FacetsDistribution, MiniFacet } from './minifacet';
+import {
+  FacetFilter,
+  // Indexable,
+  FacetsDistribution,
+  MiniFacet,
+} from './minifacet';
+
+const documents = [
+  {
+    id: 1,
+    title: 'Zen Mobs Dick',
+    text: 'Call me Ishmael. Some years ago...',
+    category: 'fiction',
+    random: 'toc',
+    review: 'good',
+    tags: ['book', 'cool'],
+  },
+  {
+    id: 2,
+    title: 'Zen and the Art of Motorcycle Maintenance',
+    text: 'I can see by my watch...',
+    category: 'fiction',
+    random: 'tic',
+    review: 'good',
+    tags: ['film', 'motor'],
+  },
+  {
+    id: 3,
+    title: 'Zen Necromancer',
+    text: 'The sky above the port was...',
+    category: 'fiction',
+    random: 'tac',
+    review: 'neutral',
+    tags: ['book', 'dark'],
+  },
+  {
+    id: 4,
+    title: 'Zen and the Art of Archery',
+    text: 'At first sight it must seem...',
+    category: 'non-fiction',
+    random: 'toc',
+    review: 'bad',
+    tags: ['film', 'dark'],
+  },
+];
 
 function macroApplyFacetFilters(
   t: ExecutionContext,
-  hits: SearchResult[],
   facetFilters: FacetFilter[],
-  expected: SearchResult[]
+  facetingFields: string[],
+  expected: number[]
 ) {
   const minifacet = new MiniFacet({
     fields: ['title', 'text'],
-    storeFields: ['title', 'category'],
-    facetingFields: ['key1', 'key2', 'key3'],
+    facetingFields: facetingFields,
   });
 
-  const filtered = minifacet.applyFacetFilters(hits, facetFilters);
+  minifacet.add(documents);
+  minifacet.compile();
 
-  t.deepEqual(expected, filtered);
+  const filtered = minifacet.applyFacetFilters(facetFilters);
+
+  t.deepEqual(expected, filtered.array());
 }
-
-const r1 = {
-  id: '1',
-  terms: ['a', 'b'],
-  score: 1,
-  match: {},
-  key1: 'val1',
-  key2: 2,
-  key3: true,
-  key4: ['attr1', 'attr2'],
-};
-const r2 = {
-  id: '2',
-  terms: ['a', 'b'],
-  score: 1,
-  match: {},
-  key1: 'val2',
-  key2: 2,
-  key3: true,
-  key4: ['attr3'],
-};
-const r3 = {
-  id: '3',
-  terms: ['a', 'b'],
-  score: 1,
-  match: {},
-  key1: 'val1',
-  key2: 2,
-  key3: false,
-  key4: ['attr1', 'attr3'],
-};
-const r4 = {
-  id: '4',
-  terms: ['a', 'b'],
-  score: 1,
-  match: {},
-  key1: 'val1',
-  key2: 3,
-  key3: true,
-  key4: ['attr4', 'attr2'],
-};
 
 test(
   'applyFacetFilters basic',
   macroApplyFacetFilters,
-  [r1, r2, r3],
-  [{ name: 'key1', values: ['val1'] }],
-  [r1, r3]
+  [new FacetFilter('review', ['good'])],
+  ['category', 'random', 'review'],
+  [0, 1]
 );
 
 test(
   'applyFacetFilters filter empty',
   macroApplyFacetFilters,
-  [r1, r2, r3],
   [],
-  [r1, r2, r3]
+  ['category', 'random', 'review'],
+  [0, 1, 2, 3]
+);
+
+test(
+  'applyFacetFilters filter inexistent value',
+  macroApplyFacetFilters,
+  [new FacetFilter('review', ['awesome'])],
+  ['category', 'random', 'review'],
+  []
 );
 
 test(
   'applyFacetFilters multiple filters',
   macroApplyFacetFilters,
-  [r1, r2, r3, r4],
   [
-    { name: 'key1', values: ['val1'] },
-    { name: 'key2', values: [2] },
-    { name: 'key3', values: [true] },
+    new FacetFilter('review', ['good']),
+    new FacetFilter('random', ['toc']),
+    new FacetFilter('category', ['fiction']),
   ],
-  [r1]
+  ['category', 'random', 'review'],
+  [0]
 );
 
 test(
   'applyFacetFilters multiple values',
   macroApplyFacetFilters,
-  [r1, r2, r3, r4],
   [
-    { name: 'key1', values: ['val1'] },
-    { name: 'key2', values: [2, 3] },
-    { name: 'key3', values: [true] },
+    new FacetFilter('random', ['toc', 'tic', 'tac']),
+    new FacetFilter('category', ['fiction']),
   ],
-  [r1, r4]
+  ['category', 'random', 'review'],
+  [0, 1, 2]
+);
+
+test(
+  'applyFacetFilters multiple values with inexistent',
+  macroApplyFacetFilters,
+  [
+    new FacetFilter('random', ['toc', 'tic', 'tac', 'tuc']),
+    new FacetFilter('category', ['fiction']),
+  ],
+  ['category', 'random', 'review'],
+  [0, 1, 2]
+);
+
+test(
+  'applyFacetFilters array facet',
+  macroApplyFacetFilters,
+  [new FacetFilter('tags', ['motor', 'book'])],
+  ['tags'],
+  [0, 1, 2]
 );
 
 function macroComputeFacetDistribution(
   t: ExecutionContext,
   attr: string[],
-  hits: SearchResult[],
+  hits: number[],
   expected: FacetsDistribution
 ) {
   const minifacet = new MiniFacet({
     fields: ['title', 'text'],
-    storeFields: ['title', 'category'],
     facetingFields: attr,
   });
+  minifacet.add(documents);
+  minifacet.compile();
 
-  const distribution = minifacet.computeFacetDistribution(hits, attr);
+  const distribution = minifacet.computeFacetDistribution(
+    new TypedFastBitSet(hits),
+    attr
+  );
 
   // t.log(distribution);
   t.deepEqual(expected, distribution);
@@ -122,116 +158,120 @@ function macroComputeFacetDistribution(
 test(
   'computeFacetDistribution basics',
   macroComputeFacetDistribution,
-  ['key1', 'key2', 'key3'],
-  [r1, r2, r3, r4],
+  ['category', 'random', 'review'],
+  [0, 1, 2, 3],
   {
-    key1: {
-      val1: 3,
-      val2: 1,
+    category: {
+      fiction: 3,
+      'non-fiction': 1,
     },
-    key2: {
-      2: 3,
-      3: 1,
+    random: {
+      toc: 2,
+      tic: 1,
+      tac: 1,
     },
-    key3: {
-      true: 3,
-      false: 1,
+    review: {
+      bad: 1,
+      good: 2,
+      neutral: 1,
     },
   }
 );
 
-test(
-  'computeFacetDistribution array',
-  macroComputeFacetDistribution,
-  ['key1', 'key2', 'key3', 'key4'],
-  [r1, r2, r3, r4],
-  {
-    key1: {
-      val1: 3,
-      val2: 1,
-    },
-    key2: {
-      2: 3,
-      3: 1,
-    },
-    key3: {
-      true: 3,
-      false: 1,
-    },
-    key4: {
-      attr1: 2,
-      attr2: 2,
-      attr3: 2,
-      attr4: 1,
-    },
-  }
-);
+// test(
+//   'computeFacetDistribution array',
+//   macroComputeFacetDistribution,
+//   ['key1', 'key2', 'key3', 'key4'],
+//   [r1, r2, r3, r4],
+//   {
+//     key1: {
+//       val1: 3,
+//       val2: 1,
+//     },
+//     key2: {
+//       2: 3,
+//       3: 1,
+//     },
+//     key3: {
+//       true: 3,
+//       false: 1,
+//     },
+//     key4: {
+//       attr1: 2,
+//       attr2: 2,
+//       attr3: 2,
+//       attr4: 1,
+//     },
+//   }
+// );
 
-test('facetedSearch', (t) => {
-  const minifacet = new MiniFacet({
-    fields: ['title', 'text'],
-    storeFields: ['title', 'category', 'random', 'tag'],
-    facetingFields: ['category', 'random', 'tag'],
-  });
+// test('facetedSearch', (t) => {
+//   const minifacet = new MiniFacet({
+//     fields: ['title', 'text'],
+//     storeFields: ['title', 'category', 'random', 'review'],
+//     facetingFields: ['category', 'random', 'review'],
+//   });
 
-  const documents = [
-    {
-      id: 1,
-      title: 'Zen Mobs Dick',
-      text: 'Call me Ishmael. Some years ago...',
-      category: 'fiction',
-      random: 'toc',
-      tag: 'good',
-    },
-    {
-      id: 2,
-      title: 'Zen and the Art of Motorcycle Maintenance',
-      text: 'I can see by my watch...',
-      category: 'fiction',
-      random: 'tic',
-      tag: 'good',
-    },
-    {
-      id: 3,
-      title: 'Zen Necromancer',
-      text: 'The sky above the port was...',
-      category: 'fiction',
-      random: 'tac',
-      tag: 'neutral',
-    },
-    {
-      id: 4,
-      title: 'Zen and the Art of Archery',
-      text: 'At first sight it must seem...',
-      category: 'non-fiction',
-      random: 'toc',
-      tag: 'bad',
-    },
-  ];
-  // Add documents to the index
-  minifacet.addAll(documents);
+//   const documents = [
+//     {
+//       id: 1,
+//       title: 'Zen Mobs Dick',
+//       text: 'Call me Ishmael. Some years ago...',
+//       category: 'fiction',
+//       random: 'toc',
+//       review: 'good',
+//     },
+//     {
+//       id: 2,
+//       title: 'Zen and the Art of Motorcycle Maintenance',
+//       text: 'I can see by my watch...',
+//       category: 'fiction',
+//       random: 'tic',
+//       review: 'good',
+//     },
+//     {
+//       id: 3,
+//       title: 'Zen Necromancer',
+//       text: 'The sky above the port was...',
+//       category: 'fiction',
+//       random: 'tac',
+//       review: 'neutral',
+//     },
+//     {
+//       id: 4,
+//       title: 'Zen and the Art of Archery',
+//       text: 'At first sight it must seem...',
+//       category: 'non-fiction',
+//       random: 'toc',
+//       review: 'bad',
+//     },
+//   ];
+//   // Add documents to the index
+//   minifacet.add(documents);
+//   minifacet.compile();
 
-  // Search for documents:
-  const results = minifacet.facetedSearch('zen', {
-    boost: { title: 2 },
-    facetFilters: [{ name: 'category', values: ['fiction'] }],
-  });
-  t.is(3, results.nbHits);
-  t.deepEqual(
-    {
-      category: {
-        fiction: 3,
-      },
-      random: {
-        tac: 1,
-        tic: 1,
-        toc: 1,
-      },
-      tag: {
-        good: 2,
-        neutral: 1,
-      },
-    },
-    results.facetsDistribution
-  );
-});
+//   // Search for documents:
+//   const results = minifacet.facetedSearch('zen', {
+//     boost: { title: 2 },
+//     facetFilters: [{ name: 'category', values: ['fiction'] }],
+//   });
+//   console.log(results);
+//   t.is(3, results.nbHits);
+//   t.deepEqual(
+//     {
+//       category: {
+//         fiction: 3,
+//       },
+//       random: {
+//         tac: 1,
+//         tic: 1,
+//         toc: 1,
+//       },
+//       review: {
+//         good: 2,
+//         neutral: 1,
+//       },
+//     },
+//     results.facetsDistribution
+//   );
+// });
--- src/lib/minifacet.ts
+++ src/lib/minifacet.ts
@@ -1,14 +1,30 @@
-import {
+import MiniSearch, {
+  // AsPlainObject,
   Options as MiniSearchOptions,
   SearchOptions,
   SearchResult,
 } from 'minisearch';
-import MiniSearch from 'minisearch';
+import TypedFastBitSet from 'typedfastbitset';
 
-export type FacetFilter = {
-  readonly name: string;
-  readonly values: readonly (string | number | boolean)[];
-};
+export type Primitives = string | number | boolean;
+
+export class FacetFilter {
+  protected name: string;
+  protected values: Primitives[];
+
+  constructor(name: string, values: Primitives[]) {
+    this.name = name;
+    this.values = values;
+  }
+
+  facetIds(): string[] {
+    return this.values.map((v) => `${this.name}:${v}`);
+  }
+}
+
+export interface Indexable {
+  readonly [attr: string]: Primitives | Primitives[];
+}
 
 export type FacetedSearchOptions = SearchOptions & {
   readonly facets?: string[];
@@ -29,78 +45,213 @@ export type Options<T> = MiniSearchOptions<T> & {
   facetingFields: string[];
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export class MiniFacet<T = any> extends MiniSearch {
-  protected _facetingFields: string[];
+export class MiniFacet<T extends Indexable> {
+  protected minisearch: MiniSearch;
+  protected facetingFields: string[];
+  // protected storeFields?: string[];
+  protected db: T[];
+  protected facetIndexes: Map<string, TypedFastBitSet>;
 
   constructor(options: Options<T>) {
-    super(options);
-    this._facetingFields = options.facetingFields;
+    this.facetingFields = options.facetingFields;
+    // this.storeFields = options.storeFields;
+    delete options.storeFields;
+    this.minisearch = new MiniSearch<T>(options);
+    this.db = [];
+    this.facetIndexes = new Map();
   }
 
-  facetedSearch(
-    queryString: string,
-    searchOptions: FacetedSearchOptions = {}
-  ): FacetedSearchResult {
-    const msResults = this.applyFacetFilters(
-      super.search(queryString, searchOptions),
-      searchOptions.facetFilters
-    );
-
-    const results: FacetedSearchResult = {
-      hits: msResults,
-      nbHits: msResults.length,
-      facetsDistribution: this.computeFacetDistribution(
-        msResults,
-        searchOptions.facets || this._facetingFields
-      ),
-    };
-
-    return results;
+  /**
+   * Adds all the given documents to the internal db
+   * This method can be called multiple times
+   *
+   * @param documents  An array of documents to be indexed
+   */
+  add(documents: T[]): void {
+    this.db.push(...documents);
   }
 
-  applyFacetFilters(
-    hits: SearchResult[],
-    facetFilters?: FacetFilter[]
-  ): SearchResult[] {
-    if (!facetFilters || facetFilters.length === 0) {
-      return hits;
-    }
+  buildFacetIndexes(): void {
+    this.db.forEach((doc, docIdx) => {
+      for (const field of this.facetingFields) {
+        const values: Primitives[] = Array.isArray(doc[field])
+          ? (doc[field] as Primitives[])
+          : ([doc[field]] as Primitives[]);
 
-    return hits.filter((r: SearchResult) => {
-      for (const filter of facetFilters) {
-        if (!r[filter.name] || !filter.values.includes(r[filter.name]))
-          return false;
-      }
-      return true;
-    });
-  }
-
-  computeFacetDistribution(
-    hits: SearchResult[],
-    facetingFields: string[]
-  ): FacetsDistribution {
-    const distribution: FacetsDistribution = Object.fromEntries(
-      facetingFields.map((a: string) => [a, {}])
-    );
-
-    hits.forEach((hit: SearchResult) => {
-      for (const field of facetingFields) {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const values: any[] = Array.isArray(hit[field])
-          ? hit[field]
-          : [hit[field]];
-
-        values.forEach((value) => {
-          if (!(value in distribution[field])) {
-            distribution[field][value] = 1;
-          } else {
-            distribution[field][value]++;
+        values.forEach((v) => {
+          const value: string = v.toString();
+          if (value) {
+            const id = `${field}:${value}`;
+            if (!this.facetIndexes.has(id)) {
+              const fbs = new TypedFastBitSet();
+              fbs.add(docIdx);
+              this.facetIndexes.set(id, fbs);
+            } else {
+              this.facetIndexes.get(id)?.add(docIdx);
+            }
           }
         });
       }
     });
+  }
+
+  /**
+   * Compile the indexes.
+   */
+  compile(): void {
+    // Add to minisearch index
+    this.minisearch.addAll(this.db);
+    // Compute facets bitmap indexes
+    this.buildFacetIndexes();
+  }
+
+  applyFacetFilters(facetFilters: FacetFilter[]): TypedFastBitSet {
+    // get all facetIndexesId
+    const facetIndexesIds = facetFilters.map((f) => f.facetIds());
+
+    const results: TypedFastBitSet = new TypedFastBitSet([...this.db.keys()]);
+
+    facetIndexesIds.forEach((filter) => {
+      const idx = filter
+        .filter((id) => this.facetIndexes.has(id))
+        .map((id) => {
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          return this.facetIndexes.get(id)!;
+        })
+        .reduce(
+          (acc: TypedFastBitSet, curr: TypedFastBitSet): TypedFastBitSet =>
+            acc.union(curr),
+          new TypedFastBitSet()
+        );
+
+      results.intersection(idx);
+    });
+
+    return results;
+    // return this.db.filter((_, i) => results.has(i));
+  }
+
+  computeFacetDistribution(
+    hits: TypedFastBitSet,
+    facetingFields: string[]
+  ): FacetsDistribution {
+    const indexes = new Map();
+
+    for (const [key, idx] of this.facetIndexes) {
+      const [field, value] = key.split(':', 2);
+
+      if (facetingFields.includes(field)) {
+        if (!indexes.has(field)) {
+          indexes.set(field, new Map());
+        }
+
+        indexes.get(field).set(value, idx);
+      }
+    }
+
+    const distribution: FacetsDistribution = Object.fromEntries(
+      facetingFields.map((field: string) => {
+        const dist: { [facetValue: string]: number } = {};
+
+        if (indexes.has(field)) {
+          for (const [key, idx] of indexes.get(field)) {
+            dist[key] = hits.new_intersection(idx).size();
+          }
+        }
+
+        return [field, dist];
+      })
+    );
+
+    // hits.forEach((hit: SearchResult) => {
+    //   for (const field of facetingFields) {
+    //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    //     const values: any[] = Array.isArray(hit[field])
+    //       ? hit[field]
+    //       : [hit[field]];
+
+    //     values.forEach((value) => {
+    //       if (!(value in distribution[field])) {
+    //         distribution[field][value] = 1;
+    //       } else {
+    //         distribution[field][value]++;
+    //       }
+    //     });
+    //   }
+    // });
 
     return distribution;
   }
+
+  // facetedSearch(
+  //   queryString: string,
+  //   searchOptions: FacetedSearchOptions = {}
+  // ): FacetedSearchResult {
+  //   const msResults = this.applyFacetFilters(
+  //     this.minisearch.search(queryString, searchOptions),
+  //     searchOptions.facetFilters
+  //   );
+
+  //   const results: FacetedSearchResult = {
+  //     hits: msResults,
+  //     nbHits: msResults.length,
+  //     facetsDistribution: this.computeFacetDistribution(
+  //       msResults,
+  //       searchOptions.facets || this.facetingFields
+  //     ),
+  //   };
+
+  //   return results;
+  // }
+
+  // getAll(): FacetedSearchResult {
+  //   console.log('minifacet::getAll');
+  //   const msResults = this.minisearch.getAll();
+
+  //   const results: FacetedSearchResult = {
+  //     hits: msResults,
+  //     nbHits: msResults.length,
+  //     facetsDistribution: this.computeFacetDistribution(
+  //       msResults,
+  //       this.facetingFields
+  //     ),
+  //   };
+
+  //   return results;
+  // }
+
+  // toJSON(): AsPlainObject {
+  //   return this.minisearch.toJSON();
+  //   // return {
+  //   //   ...this.minisearch.toJSON(),
+  //   //   ...{ facetingFields: this.facetingFields },
+  //   // };
+  // }
+
+  // // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // static loadJSON<T = any>(json: string, options: Options<T>): MiniFacet<T> {
+  //   const miniFacet = new MiniFacet(options);
+  //   const minisearch = MiniSearch.loadJSON(json, options) as MiniSearch;
+  //   miniFacet.minisearch = minisearch;
+  //   return miniFacet;
+  // }
+
+  // // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // static async fetchJSON<T = any>(
+  //   url: string,
+  //   options: Options<T>
+  // ): Promise<MiniFacet<T>> {
+  //   const response = await window.fetch(url);
+
+  //   if (response.ok) {
+  //     const json = await response.json();
+  //     // TODO: check json de serialization OK
+  //     const miniFacet = new MiniFacet(options);
+  //     const minisearch = MiniSearch.loadJS(json, options);
+  //     miniFacet.minisearch = minisearch as MiniSearch;
+  //     return miniFacet;
+  //   } else {
+  //     return Promise.reject(response.statusText);
+  //   }
+  // }
 }

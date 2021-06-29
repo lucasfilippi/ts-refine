c775a38c8bf6abc2223377f4e12f96ffba779a91
Lucas Filippi <lucasfilippi@protonmail.com>
2021-06-29 15:55:09 +0200

    Add GeoINdex support

---
+++ src/lib/geoindex.spec.ts
@@ -0,0 +1,69 @@
+import test, { ExecutionContext } from 'ava';
+import TypedFastBitSet from 'typedfastbitset';
+
+import { GeoIndex, GeoWithinBox } from './geoindex';
+const pointOfInterest = [
+  {
+    name: 'Le louvre',
+    description: `Le musée du Louvre est un musée situé dans le 1ᵉʳ arrondissement de Paris, en France. Une préfiguration en est imaginée en 1775-1776 par le comte d'Angiviller, directeur général des Bâtiments du roi, comme lieu de présentation des chefs-d'œuvre de la collection de la Couronne.`,
+    coordinates: [48.860052, 2.336072],
+    tags: ['museum', 'monument'],
+  },
+  {
+    name: 'Arc de Triomphe',
+    description: `L'arc de triomphe de l’Étoile, souvent appelé simplement l'Arc de Triomphe, est un monument situé à Paris, en un point haut à la jonction des territoires des 8e, 16e et 17e arrondissements`,
+    coordinates: [48.861697, 2.332939],
+    tags: ['monument'],
+  },
+  {
+    name: 'Le sacré coeur',
+    description: `La basilique du Sacré-Cœur de Montmartre, dite du Vœu national, située au sommet de la butte Montmartre, dans le quartier de Clignancourt du 18e arrondissement de Paris (France), est un édifice religieux parisien majeur, « sanctuaire de l'adoration eucharistique et de la miséricorde divine » et propriété de l'archidiocèse de Paris1.`,
+    coordinates: [48.886183, 2.34311],
+    tags: ['monument', 'church'],
+  },
+  {
+    name: 'Père-lachaise',
+    description: `Le cimetière du Père-Lachaise est le plus grand cimetière parisien intra muros et l'un des plus célèbres dans le monde. Situé dans le 20ᵉ arrondissement, de nombreuses personnes célèbres y sont enterrées.`,
+    coordinates: [48.85989, 2.389177],
+    tags: ['place', 'graveyard'],
+  },
+  {
+    name: 'Disneyland paris',
+    description: `Disneyland Paris, anciennement Euro Disney Resort puis Disneyland Resort Paris, est un complexe touristique et urbain de 22,30 km² situé en sa majeure partie sur la commune de Chessy, à trente-deux kilomètres à l'est de Paris.`,
+    coordinates: [48.867208, 2.783858],
+    tags: ['park', 'amusement'],
+  },
+  {
+    name: 'Notre dame de reims',
+    description: `La cathédrale Notre-Dame de Reims, est une cathédrale catholique romaine située à Reims, dans le département français de la Marne en région Grand Est. Elle est connue pour avoir été, à partir du XIᵉ siècle, le lieu de la quasi-totalité des sacres des rois de France.`,
+    coordinates: [49.253418, 4.033719],
+    tags: ['church'],
+  },
+  {
+    name: 'Memorial Verdun',
+    description: `Le Mémorial de Verdun est un musée consacré à l'histoire et à la mémoire de la bataille de Verdun de 1916, situé à Fleury-devant-Douaumont, à quelques kilomètres de Verdun, dans le département de la Meuse en région Grand Est`,
+    coordinates: [49.194235, 5.433743],
+    tags: ['place', 'graveyard'],
+  },
+  {
+    name: 'Notre dame de Strasbourg',
+    description: `La cathédrale Notre-Dame de Strasbourg est une cathédrale gothique située à Strasbourg, dans la circonscription administrative du Bas-Rhin, sur le territoire de la collectivité européenne d'Alsace.`,
+    coordinates: [48.581454, 7.750879],
+    tags: ['church'],
+  },
+];
+
+test('geoindex ', async (t) => {
+  const idx = new GeoIndex({
+    field: 'coordinates',
+  });
+
+  idx.build(pointOfInterest);
+
+  const results = await idx.search(
+    new TypedFastBitSet([0, 1, 2, 3, 4, 5, 6, 7]),
+    new GeoWithinBox([48, 2], [50, 3])
+  );
+
+  t.deepEqual(results.ids.array(), [0, 1, 2, 3, 4]);
+});
---
+++ src/lib/geoindex.ts
@@ -0,0 +1,111 @@
+import TypedFastBitSet from 'typedfastbitset';
+
+import { Index, Indexable, IndexSearchResult } from './embexed';
+import { GeoKDBush } from './geokdbush';
+
+export type BuildOptions = {
+  field: string;
+};
+
+export type Coordinates = [long: number, lat: number];
+
+enum GeoOperationKind {
+  GeoWithinSphere = 1,
+  GeoWithinBox,
+  GeoAround,
+}
+
+export interface Operation {
+  kind: GeoOperationKind;
+  execute(index: GeoKDBush<Coordinates>): number[];
+}
+
+export class GeoWithinSphere implements Operation {
+  kind = GeoOperationKind.GeoWithinSphere;
+  protected center: Coordinates;
+  protected radius: number;
+
+  constructor(center: Coordinates, radius: number) {
+    this.center = center;
+    this.radius = radius;
+  }
+
+  execute(index: GeoKDBush<Coordinates>): number[] {
+    return index.within(this.center[0], this.center[1], this.radius);
+  }
+}
+
+export class GeoWithinBox implements Operation {
+  kind = GeoOperationKind.GeoWithinBox;
+  protected min: Coordinates;
+  protected max: Coordinates;
+
+  constructor(min: Coordinates, max: Coordinates) {
+    this.min = min;
+    this.max = max;
+  }
+
+  execute(index: GeoKDBush<Coordinates>): number[] {
+    return index.range(this.min[0], this.min[1], this.max[0], this.max[1]);
+  }
+}
+
+export class GeoAround implements Operation {
+  kind = GeoOperationKind.GeoAround;
+  protected center: Coordinates;
+  protected radius?: number;
+  protected maxResults?: number;
+
+  constructor(center: Coordinates, radius?: number, maxResults?: number) {
+    this.center = center;
+    this.radius = radius;
+    this.maxResults = maxResults;
+  }
+
+  execute(index: GeoKDBush<Coordinates>): number[] {
+    return index.around(
+      this.center[0],
+      this.center[1],
+      this.maxResults,
+      this.radius
+    );
+  }
+}
+
+export class GeoIndex implements Index {
+  protected field: string;
+  protected index?: GeoKDBush<Coordinates>;
+
+  constructor(options: BuildOptions) {
+    this.field = options.field;
+  }
+
+  get key(): string {
+    return 'geo_' + this.field.toLowerCase();
+  }
+  // add some documents to the index
+  build(documents: Indexable[]): void {
+    this.index = new GeoKDBush<Coordinates>(
+      documents.map((d) => {
+        return d[this.field] as Coordinates;
+      })
+    );
+  }
+
+  async search(
+    on: TypedFastBitSet,
+    operation: Operation
+  ): Promise<IndexSearchResult> {
+    return {
+      ids: on.new_intersection(
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        new TypedFastBitSet(operation.execute(this.index!))
+      ),
+      // metadata?: Map<number, Metadata>; // ids indexed metadata
+    };
+  }
+
+  serialize(): string {
+    return '';
+  }
+}
